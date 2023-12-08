60c0501b5de9b05ae1f48cea274814ec338e9712
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2023-12-08 11:00:45 -0300

    Api projeto Event Plus

---
+++ Event+/webapi.event+/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using webapi.event_.Domains;
+
+namespace webapi.event_.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(Usuario usuario);
+     
+        Usuario BuscarPorId(Guid id);
+
+        Usuario BuscarPorEmailESenha(string email, string senha);
+
+        void Deletar (Guid id);
+    }
+}
